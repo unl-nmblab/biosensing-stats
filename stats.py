fd43e51b24d527ed6cb1327968e47a61d7e92393
Chase Hermreck <79621518+chase-hermreck@users.noreply.github.com>
2021-02-25 01:13:43 -0600

    Added file dialog button

--- stats.py
+++ stats.py
@@ -16,8 +16,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
 import tkinter as tk
+from tkinter import filedialog
+
+def open_file_dialog():
+    filepath = tk.filedialog.askopenfilename(initialdir = "./", title = "Select a File", filetypes = (("Text files", "*.txt*"), ("All files", "*.*")))
 
 window = tk.Tk()
 window.title("biosensing-stats")
 window.geometry("700x450")
+button_file_dialog = tk.Button(window, height = 1, width = 10, text = "Browse Files", command = open_file_dialog)
+button_file_dialog.place(x = 10, y = 10)
 window.mainloop()
