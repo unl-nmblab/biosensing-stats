d7e7fac21b59fd9ada4e02e133282d745adfb974
Chase Hermreck <79621518+chase-hermreck@users.noreply.github.com>
2021-02-25 01:10:33 -0600

    Added display window

--- stats.py
+++ stats.py
@@ -14,3 +14,10 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
+
+import tkinter as tk
+
+window = tk.Tk()
+window.title("biosensing-stats")
+window.geometry("700x450")
+window.mainloop()
